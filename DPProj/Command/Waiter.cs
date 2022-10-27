e9c909dd62c3be63e18bf8b2ff2524770b60cdf5
MiriShamur <100590128+MiriShamur@users.noreply.github.com>
2022-10-27 12:09:43 +0300

    Update Waiter.cs

--- DPProj/Command/Waiter.cs
+++ DPProj/Command/Waiter.cs
@@ -20,17 +20,11 @@ namespace DPProj.Command
         public void ExecuteSendOrder(Order o)
         {
             o.Execute(this);
-           /* if (o.CurrentState.State is FState.Canceled)
-            {
-                Debug.Print("canceld");
-                return ;
-            }*/
         }
         public override void Update(Product product,Order o)
         {
            o.CurrentState.State.Handle(o.CurrentState);
            Debug.Print($"מנה מספר {o.OrderNumber} של{o.Name} מוכנה");
-           // Cook.Detach(this);
         }
     }
 }
